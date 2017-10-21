5515f99b5f4d31731bb1e1b64ddd2b35ffc3b280
julien1bastin <julienbastin96@hotmail.com>
2017-10-21 22:44:52 +0200

    ajout jacobson

---
+++ src/jacobson.h
@@ -0,0 +1,10 @@
+// ============================== JACOBSON ALGORITHM CONSTANTS ==============================
+
+#define ALPHA 0.875
+#define PARA_G 0.125
+#define PARA_H 0.25
+#define PARA_F 4
+#define SRTT_0 1.5
+#define SDEV_0 0
+
+float compute_retransmission_time(float);
